6caec424d87cd53ea15b7efa2842df1be8f66801
Janne <immonenjanne@icloud.com>
2022-03-01 14:15:42 +0200

    Initial Commit

---
+++ HistoryQuizApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HistoryQuizApp
+//
+//  Created by Janne Immonen on 1.3.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HistoryQuizApp/HistoryQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryQuizAppApp.swift
+//  HistoryQuizApp
+//
+//  Created by Janne Immonen on 1.3.2022.
+//
+
+import SwiftUI
+
+@main
+struct HistoryQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
